574aaaa00c18d4e90feeacc4ff7d4bb204dc4a43
Valdemar1902 <Valdemar1902@gmail.com>
2025-02-03 07:27:27 +0100

    opg3

---
+++ mit-console-projekt/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(Opgave3.Faculty(5)); // Output skal være '120'.
+
+class Opgave3 {
+    public static int Faculty(int n)
+    {
+        if (n == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * Faculty(n - 1);
+        }
+    }
+}
+    
+ 
